3a20a51d32100beca56279f63f38587d508d96b4
Gavobro <gavalobrien@gmail.com>
2022-11-24 19:38:12 -0400

    Updated script.js - Created the "saveBtn" variable and pairing it with the class from the index.html. - Can now save the entered text within the "description" to the local storage for later use. - Can now save the entered text within the "description" even when the page is refreshed. - Added fuction.

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,3 +1,28 @@
 // Added date and time based on the current day.
 $("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
  
+// Created the "saveBtn" variable and pairing it with the class from the index.html.
+var saveBtn = $ (".saveBtn") ;
+
+// Saving the entered text within the "description" to the local storage for later use.
+saveBtn.on ("click", function () {
+
+  var time = $ (this) .siblings (".hour") .text () ;
+  var description = $ (this) .siblings (".description") .val () ;
+  localStorage.setItem (time, description) ;
+
+}) ;
+
+// Saving the entered text within the "description" even when the page is refreshed.
+function usePlanner () {
+    $ (".hour") .each (function () {
+      var collumn = $ (this) .text ();
+      var desc = localStorage.getItem (collumn) ;
+  
+      if (desc !== null) {
+        $ (this).siblings (".description") .val (desc) ;
+      }
+    }) ;
+  }
+// Fuction used to keep text when the page is refreshed.
+usePlanner();
