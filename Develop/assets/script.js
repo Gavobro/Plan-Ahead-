e93126da70a85dc9982a5318bbea9836a191e30b
Gavobro <gavalobrien@gmail.com>
2022-11-24 20:00:10 -0400

    Updated script.js - Cleaned up a few lines of code with proper spacing - Added comments - Made collumn colours based off of the current time using "future", "present", and "past" ids from the html. - Added new funtion used for cullumn colour change in relation to time.

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,5 +1,5 @@
 // Added date and time based on the current day.
-$("#currentDay").text(moment().format("dddd MMMM Do YYYY"));
+$ ("#currentDay") .text (moment () .format ("dddd MMMM Do YYYY") );
  
 // Created the "saveBtn" variable and pairing it with the class from the index.html.
 var saveBtn = $ (".saveBtn") ;
@@ -16,13 +16,41 @@ saveBtn.on ("click", function () {
 // Saving the entered text within the "description" even when the page is refreshed.
 function usePlanner () {
     $ (".hour") .each (function () {
-      var collumn = $ (this) .text ();
-      var desc = localStorage.getItem (collumn) ;
+        var collumn = $ (this) .text ();
+        var desc = localStorage.getItem (collumn) ;
   
-      if (desc !== null) {
-        $ (this).siblings (".description") .val (desc) ;
-      }
+        if (desc !== null) {
+            $ (this) .siblings (".description") .val (desc) ;
+        }
     }) ;
   }
+
+// Making cullumn colours based off of the current time using the "future", "present", and "past" ids.
+function timeBlockColor () {
+    var hour = moment () .hours () ;
+
+    $ (".time-block") .each (function () {
+        var collumn = parseInt ($ (this) .attr ("id") ) ;
+
+// If the collumn number is more than the current time, then the "future" colour scheme is selected.
+        if (collumn > hour) {
+            $ (this) .addClass ("future") ;
+        } 
+
+// If the collumn number is equal to the current time, then the "present" colour scheme is selected.      
+        else if (collumn === hour) {
+            $ (this) .addClass ("present") ;
+        } 
+        else {
+
+// If the collumn number is less than the current time, then the "past" colour scheme is selected.      
+            $ (this) .addClass ("past") ;
+        }
+    })
+} ;
+
 // Fuction used to keep text when the page is refreshed.
-usePlanner();
+usePlanner () ;
+
+// Funtion used for cullumn colour change in relation to time.
+timeBlockColor () ;
